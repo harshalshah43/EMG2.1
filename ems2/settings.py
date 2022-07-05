99dd639effed9a679abf79ea88e236e90d4a7dd4
Harshal Shah <65274384+harshalshah43@users.noreply.github.com>
2022-07-05 11:21:22 +0530

    Update settings.py

--- ems2/settings.py
+++ ems2/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=45iceym-cznn2)lh8k$_*$tvq!6(z_tq1kb)8s9h6ntm_)&gn'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
